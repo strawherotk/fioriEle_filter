797afe8007aa83b59f359dca86ff0eb6ef4a17d7
Vuong Ho <vuong.hoquoc@vn.bosch.com>
2021-10-28 02:56:45 +0000

    [Vuong] Project from template & added first filter

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("com.hirose.sd.price.pricecondreport.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
